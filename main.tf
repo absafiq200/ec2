7f6c5df299ead0037a555dd712d0be07ddf44b13
shafeeq.bari <shafeeq.bari@dish.com>
2022-09-27 08:52:33 -0600

    initial

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
+locals {
+    arn = ""
+}
+
 module "my_ec2_main" {
   source         = "./modules/ec2"
   key_pair       = var.my_key_pair
   pub_ip_address = var.my_pub_ip_address
   subnet_id      = var.my_subnet_id
   sec_group      = var.my_sec_group
-
+  local.arn = module.my_ec2.arn
+  
 }
+
--- output.tf
+++ output.tf
@@ -5,10 +5,7 @@ output "my_ec_id_2" {
 
 output "my_ec_inf" {
   description = "my ec2 instance info"
-  value       = module.my_ec2_main.my_ec2
+  value       = local.arn
 }
 
-output "my_ec_inf2" {
-  description = "my ec2 instance info"
-  value       = module.my_ec2_main.aws_instance.my_ec2
-}
+
